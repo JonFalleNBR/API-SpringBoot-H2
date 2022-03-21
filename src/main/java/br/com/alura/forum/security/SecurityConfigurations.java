1f56d19a9a1ba7addc2d5678f7bdbf27ff81f8b5
Jonathan Lopes <jonathan.lopessilva@emeal.nttdata.com>
2022-03-21 18:27:21 -0300

    Security - Configuração do Metodo http com acesso permitido ao tipo GET

--- src/main/java/br/com/alura/forum/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,6 +12,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    //Configuração de Autorização -> Controle de acesso
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+    }
+    //Configurações de Recursos Estáticos - Requisições para artigos, javaScript, Imagens, etc
+    @Override
+    public void configure(WebSecurity web) throws Exception {
 
+    }
+    //Configurações de Autenticação - Login, acesso, etc
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+    }
 }
