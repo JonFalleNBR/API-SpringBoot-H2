1314fda2f308b759825e0aa60afd27c9376c3d64
Jonathan Lopes <jonathan.lopessilva@emeal.nttdata.com>
2022-03-23 16:34:37 -0300

    Security - Configurações complexas na autenticação | criação de classes, Interfaces e melhorias nos metodos de autenticacao ja criados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$TXT/qLS6Z1Cw99g2zGg4b.n5jQh9x1iJlOgDTIOn1xIRU39UfiWZm');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
